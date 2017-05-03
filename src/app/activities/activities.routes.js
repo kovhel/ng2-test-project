6ccdd3c5235e348ebfdc660e13788c4b2d2934d6
Helen Koval <elena.koval.ukraine@gmail.com>
2017-05-04 01:46:36 +0300

    remove compiled files, add them to gitignore

--- src/app/activities/activities.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-var activities_component_1 = require("./activities.component");
-var routes = [
-    { path: '', component: activities_component_1.default },
-];
-exports.default = router_1.RouterModule.forChild(routes);
-//# sourceMappingURL=activities.routes.js.map
--- src/app/home/home.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-var home_component_1 = require("./home.component");
-var routes = [
-    { path: '', component: home_component_1.default },
-];
-exports.default = router_1.RouterModule.forChild(routes);
-//# sourceMappingURL=home.routes.js.map
